cc0f0791355e8a12a219484bdd8710e0c08c9612
test1 <s2mos2@naver.com>
2019-03-27 23:49:22 +0900

    Updates

---
+++ precache-manifest.46917ccc501d93cd04bc62f38edfb684.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  },
+  {
+    "revision": "cd0862d3e1cd334ae8af",
+    "url": "/movie_app/static/js/main.cc0dc2c7.chunk.js"
+  },
+  {
+    "revision": "f26ae2ba1199b685be6b",
+    "url": "/movie_app/static/js/2.1d254b63.chunk.js"
+  },
+  {
+    "revision": "cd0862d3e1cd334ae8af",
+    "url": "/movie_app/static/css/main.bed3efa1.chunk.css"
+  },
+  {
+    "revision": "4b4f8ef93fbf9cfddda0c4fa328ace36",
+    "url": "/movie_app/index.html"
+  }
+];
